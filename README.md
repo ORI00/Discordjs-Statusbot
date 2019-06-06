c61aa924db6943a5ac215b7f8febf655def81b4c
zenx <support@clusternetworking.com>
2019-06-06 16:42:51 +0200

    Added more status types

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Simple discordjs bot which lets you easily set a custom game status.
 
+# Changelog:
+05/06/2019 First release
+06/06/2019 Added more types of the game status
+
 # How to use:
 
 How it works:
@@ -9,6 +13,6 @@ How it works:
 2) Create config.json {"token": "yourdiscordbottoken"} in same folder as your app.js
 3) Replace 'yourtext' in setActivity to whatever you want your game status to be
 4) Open console: node app.js  (or your js file name)
-4) You will now have your custom game status, example: http://prntscr.com/ny2k9k
+4) You will now have your custom game status, example: http://prntscr.com/nyfxyh
 
 
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //      How it works:
 //      1) npm install discord.js
 //      2) Create config.json {"token": "yourdiscordbottoken"} in same folder as your app.js
-//      3) Replace 'yourtext' in setActivity to whatever you want your game status to be
+//      3) Replace 'bot by zenx#0001' in setActivity to whatever you want your game status to be
 //      4) Open console: node app.js  (or your js file name)
-//      4) You will now have your custom game status, example: http://prntscr.com/ny2k9k
+//      5) You will now have your custom game status, example: http://prntscr.com/nyfxyh
 
 
 
@@ -12,8 +12,22 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
 
+
 //Set your game activity
-bot.on('ready', () => { bot.user.setActivity('yourtext') })
+bot.on(`ready`, () => {
+
+    bot.user.setPresence({
+        status: `dnd`,       //possible status: online, idle, offline, dnd
+        game: {
+            name: "Bot by zenx#0001",
+            type: "PLAYING"
+        }
+    })
+
+});
+
+
+
 
 //Text on successfull launch.
 console.log('Custom status has been succesfully launched.');
@@ -21,3 +35,9 @@ console.log('Custom status has been succesfully launched.');
 
 //Loads token from config.json   http://prntscr.com/ny2fh1 example
 bot.login(config.token);
+
+
+
+
+
+
